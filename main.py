de467443e40d803c70a0aa8c824224ead7f9e971
Cristian Viracachá <89107553+cfvr13@users.noreply.github.com>
2021-11-09 10:49:26 -0500

    imprimir en python

---
+++ main.py
@@ -0,0 +1,16 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+
+string_var = "Hola clase de programacion"
+integer_var = 123
+float_var =123.24
+
+print(string_var)
+print(type (string_var))
+
+print(integer_var)
+print(type(integer_var))
+
+print(float_var)
+print(type(float_var))
